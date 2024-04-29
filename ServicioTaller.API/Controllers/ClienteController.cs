86563a118de2cb4c77f2e1a6694f0b4fc2723be1
JoseRamirezAlutelMobility <129778654+JoseRamirezAlutelMobility@users.noreply.github.com>
2024-04-29 10:41:59 -0600

    Primer commit

---
+++ ServicioTaller.API/Controllers/ClienteController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ServicioTaller.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClienteController : ControllerBase
+    {
+
+    }
+}
